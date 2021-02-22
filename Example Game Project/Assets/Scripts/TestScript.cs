3ea540a18de3e1f7fb52dc60cec27223a327f7f8
SarahHerzog <sarahherzog@fife.ac.uk>
2021-02-22 12:52:57 +0000

    Test script for learning scripting basics

---
+++ Example Game Project/Assets/Scripts/TestScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    // Variables that will be used in Unity
+    public string startText;
+    public string updateText;
+
+
+    // Start is called before the first frame update 
+    void Start()
+    {
+        Debug.LogWarning(startText);
+        Debug.LogError(startText);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(updateText);
+    }
+}
